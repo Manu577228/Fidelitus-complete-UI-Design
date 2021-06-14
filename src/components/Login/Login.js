14bfb1a61ebe35eb3649ec136dc16c70a4335aad
Manubharadwaj5772 <84725175+Manubharadwaj5772@users.noreply.github.com>
2021-06-14 16:04:05 +0530

    project

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,19 +4,28 @@ import './Login.css'
 function Login() {
     const [styleme, setstyleme] = useState(false)
     return (
-        <div className="login_container">
+<div className="login_container">
 
- <div className={!styleme ?"login-left":"login-left1"}>
- <button onClick={()=>setstyleme(!styleme)}>Forget Password</button></div>
- 
- 
+<div className={!styleme ?"login-left":"login-left1"}> 
+<div className = "text"> 
+         <p className = "welcome"> WELCOME BACK </p>
+         <p className = "happy"> We're happy to see you again </p>
+         <button className= "btn-1" type="button"  onClick={()=>setstyleme(!styleme)}>FORGOT PASSWORD</button>
+       
+    </div>
+</div>
+
  <div className={!styleme ? "login-right":"login-right1"}>
-    <div className={!styleme ? "loginbox-":"loginbox1"}>
 
-        </div>
+
+<div className={!styleme ? "loginbox-":"loginbox1"}>
+
+
+       </div>          
     </div>            
 </div>
-    )
+
+    );
 }
 
 export default Login
